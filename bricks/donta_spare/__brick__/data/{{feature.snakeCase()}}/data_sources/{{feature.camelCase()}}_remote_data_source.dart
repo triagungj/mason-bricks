cd2335f51b2a1552200dfa2b89b9f668f9ad1c9c
tj.varx <tri.agung@varx.id>
2023-09-11 23:07:23 +0700

    refactor: structures

--- bricks/donta_spare/__brick__/data/{{feature.snakeCase()}}/data_sources/{{feature.camelCase()}}_remote_data_source.dart
+++ bricks/donta_spare/__brick__/data/{{feature.snakeCase()}}/data_sources/{{feature.camelCase()}}_remote_data_source.dart
@@ -1,4 +1,31 @@
+import 'package:dio/dio.dart';
 
 abstract class {{ feature.pascalCase() }}RemoteDataSource { {{ #usecases }}
   Future<{{ response.pascalCase() }}Entity> {{ method }}{{ name.pascalCase() }}({{ #isHaveBody }}{{ bodyName.pascalCase() }} body{{ /isHaveBody }});{{ /usecases }}
+}
+
+class {{ feature.pascalCase() }}RemoteDataSourceImpl implements {{ feature.pascalCase() }}RemoteDataSource {
+  {{ feature.pascalCase() }}RemoteDataSourceImpl(this._client);
+
+  final Dio _client;
+  {{ #usecases }}
+  @override
+  Future<{{ response.pascalCase() }}Entity> {{ method }}{{ name.pascalCase() }}({{ #isHaveBody }}{{ bodyName.pascalCase() }} body{{ /isHaveBody }}) async {
+    try {
+      final response = await _client.{{ method.snakeCase() }}<Map<String, dynamic>>(
+        'url', {{ #isHaveBody }}
+        // TODO Add body {{ /isHaveBody }}
+      );
+      return {{ response.pascalCase() }}Model.fromJson(response.data!);
+    } on DioException catch (e) {
+      throw DioException(
+        requestOptions: e.requestOptions,
+        error: e.error,
+        response: e.response,
+      );
+    } catch(e) {
+      throw ServerException();
+    }
+  }
+  {{ /usecases }}
 }
